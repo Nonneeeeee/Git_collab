c66b36186cc08773ded24495045e43b20c7a0959
Ponradee <peeradolxa.123@gmail.com>
2026-09-23 17:21:41 +0700

    dfdsf

--- Hello.c
+++ Hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
+
+    greeting("Developer A,B,C !!");
     printf("Hello, Git collabaration!\n");
     return 0;
+}
+
+int greeting(const char *name)
+{
+printf("Hello, %s!\n", name);
+return 0;
 }
