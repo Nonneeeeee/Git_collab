29e729b7b2b77d1eefbd0a3445b62defc490ed60
Nonneeeeee <153512613+Nonneeeeee@users.noreply.github.com>
2026-09-23 17:51:47 +0700

    Refactor main function and add farewell function

--- Hello.c
+++ Hello.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-int main(){
-
+int main(void)
+{
     greeting("Developer A,B,C !!");
-    printf("Hello, Git collabaration!\n");
+    farewell("Developer A,B,C");
     return 0;
 }
 // Print a greeting message for the specified user
 int greeting(const char *name)
 {
-printf("Hello, %s!\n", name);
-return 0;
+    printf("Hello, %s!\n", name);
+    return 0;
+}
+
+int farewell(const char *name)
+{
+    printf("Goodbye, %s!\n", name);
+    return 0;
 }
