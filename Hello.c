ede29c57404c362a71fe27c779ae9b47d7a1d422
Nonnee <nonneetk@gmail.com>
2026-09-23 17:57:32 +0700

    add checkin

--- Hello.c
+++ Hello.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello, Git collabaration!\n");
+int main(void)
+{
+    checkin("Developer A,B,C");
+    greeting("Developer A,B,C !!");
+    farewell("Developer A,B,C");
+    return 0;
+}
+// Print a greeting message for the specified user
+int greeting(const char *name)
+{
+    printf("Hello, %s!\n", name);
+    return 0;
+}
+
+int farewell(const char *name)
+{
+    printf("Goodbye, %s!\n", name);
+    return 0;
+}
+
+int checkin(const char *name)
+{
+    printf("%s are checked in\n", name);
     return 0;
 }
