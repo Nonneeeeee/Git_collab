8d63635bf5afead14bc0a21a4a5599b0f8d495f6
jidapachan <69010143@kmitl.ac.th>
2026-09-23 17:42:21 +0700

    sixnine

--- Hello.c
+++ Hello.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
+
 int main(void)
 {
 greeting("Developer A,B,C !!");
 farewell("Developer A,B,C");
+return 0;
+}
+// Print a greeting message for the specified user
+int greeting(const char *name)
+{
+printf("Hello, %s!\n", name);
 return 0;
 }
 int farewell(const char *name)
